0f6e609b59a8e05d971e2c96a5a6b0f18f46086b
michasdev <michel.freitas@r2ventures.com.br>
2026-04-16 01:59:17 -0300

    feat(desktop): add layout, instances page base, and fix colors

--- apps/desktop/src/renderer/src/app/router.tsx
+++ apps/desktop/src/renderer/src/app/router.tsx
@@ -1,13 +1,16 @@
 import { createHashRouter } from 'react-router'
 import NotFoundPage from '@renderer/app/pages/not-found'
 import ResourcesPage from '@renderer/features/resources/resources'
+import InstancesPage from '@renderer/features/instances/instances'
+import { Layout } from '@renderer/shared/layout'
 
 export const router = createHashRouter([
   {
     path: '/',
-    element: <ResourcesPage />,
+    element: <Layout />,
     children: [
-      { index: true, element: <ResourcesPage /> },
+      { index: true, element: <InstancesPage /> },
+      { path: '/resources', element: <ResourcesPage /> },
       { path: '*', element: <NotFoundPage /> },
     ],
   },
--- apps/desktop/src/renderer/src/features/instances/instances.tsx
+++ apps/desktop/src/renderer/src/features/instances/instances.tsx
@@ -1,9 +1,30 @@
-const ResourcesPage = () => {
+import {
+    Frame,
+    FrameDescription,
+    FrameFooter,
+    FrameHeader,
+    FramePanel,
+    FrameTitle,
+} from "@/components/ui/frame";
+
+const InstancesPage = () => {
     return (
         <>
-            <h1>Instances</h1>
+            <Frame className="w-full">
+                <FrameHeader>
+                    <FrameTitle>Instances</FrameTitle>
+                    <FrameDescription>Manage your MildStack instances</FrameDescription>
+                </FrameHeader>
+                <FramePanel>
+                    <h2 className="font-semibold text-sm">Instance #1</h2>
+                    <p className="text-muted-foreground text-sm">Running</p>
+                </FramePanel>
+                <FrameFooter>
+                    <p className="text-muted-foreground text-sm">Footer</p>
+                </FrameFooter>
+            </Frame>
         </>
     )
 }
 
-export default ResourcesPage;
+export default InstancesPage;
