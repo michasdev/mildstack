9da0351f0db6d8a3453d886a0efb47124d9bad26
michasdev <contato@michelfreitas.com>
2026-04-18 04:03:30 -0300

    feat: upload base website

---
+++ apps/web/www/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+export function App() {
+  return (
+    <div className="flex min-h-svh p-6">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+        <div>
+          <h1 className="font-medium">Mildstack: lightweight AWS local emulator</h1>
+          <p>Under development. GitHub: <a href="https://github.com/michasdev/mildstack/"><pre>https://github.com/michasdev/mildstack/</pre></a></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
